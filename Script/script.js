8d88770196a745b89f40eacff127bc5bef40ddc5
Kat-Krich <katarina.krich@gmail.com>
2025-04-15 14:49:29 -0300

    .

--- Script/script.js
+++ Script/script.js
@@ -1,2 +1,14 @@
 let canva1 = document.getElementById("canva1");
 let ctx = canva1.getContext("2d");
+
+let tela1 = {
+    x: 0,
+    y: 0,
+    width: 1000,
+    height: 600,
+    img: new Image(),
+}
+
+function primeira_tela(){
+    
+}
