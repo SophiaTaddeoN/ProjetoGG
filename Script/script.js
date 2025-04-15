bcb2bffeb3b0c67f0c67d7833696b10f6be9d4d7
Kat-Krich <katarina.krich@gmail.com>
2025-04-15 16:57:46 -0300

    ..

--- Script/script.js
+++ Script/script.js
@@ -1,14 +1,21 @@
 let canva1 = document.getElementById("canva1");
 let ctx = canva1.getContext("2d");
 
-let tela1 = {
-    x: 0,
-    y: 0,
-    width: 1000,
-    height: 600,
-    img: new Image(),
+class telas {
+    constructor(url_img, x, y, width, heigh) {
+        this.img = new Image();
+        this.url_img = url_img;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.heigh = heigh;
+    }
+    desenhe(){
+        this.img.src = this.url_img;
+        ctx.beginPath();
+        ctx.drawImage(this.x, this.y, this.width, this.heigh);
+        ctx.closePath();
+    }
 }
 
-function primeira_tela(){
-    
-}
+let tela_1 = new telas('Imagens/praia.jpeg', 0, 0, 1000, 600);
