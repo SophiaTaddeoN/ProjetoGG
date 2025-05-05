e44913060fa5ff0ff91e836dd451dbbe32e218f3
Kat-Krich <katarina.krich@gmail.com>
2025-05-05 16:56:59 -0300

    Atualizações

--- Script/script.js
+++ Script/script.js
@@ -20,5 +20,36 @@ class Telas {
     }
 }
 
+class Objetos {
+    constructor(url_img, x, y, width, height) {
+        this.img = new Image();
+        this.url_img = url_img;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    desenhe(){
+        this.img.onload = () => {
+            ctx.beginPath();
+            ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+            ctx.closePath();
+        }
+        this.img.src = this.url_img;
+    }
+}
+
 let tela_1 = new Telas('Imagens/praia.jpeg', 0, 0, 1000, 600);
 tela_1.desenhe();
+let tela_2 = new Telas('Imagens/fundo-cabana.jpeg', 0, 0, 1000, 600);
+tela_2.desenhe();
+
+let bancada_cozinha2 = new Objetos('Imagens/bancada-cozinha2.png', 900, 120, 100, 160);
+let fogao = new Objetos('Imagens/fogao.png', 802, 120, 100, 110)
+let bancada_cozinha1 = new Objetos('Imagens/bancada-cozinha1.png', 553, 120, 250, 160);
+let geladeira = new Objetos('Imagens/geladeira.png', 458, 80, 100, 150)
+let cama = new Objetos('Imagens/cama.png', 40, 90, 170, 228)
+bancada_cozinha2.desenhe();
+bancada_cozinha1.desenhe();
+fogao.desenhe();
+geladeira.desenhe();
