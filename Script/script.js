1c7e43d299bddc4be6002861c67282b3ffa20ac1
Kat-Krich <katarina.krich@gmail.com>
2025-05-09 16:35:08 -0300

    mudanças

--- Script/script.js
+++ Script/script.js
@@ -43,29 +43,32 @@ class Objetos {
     }
 }
 
-let tela_1 = new Telas('Imagens/praia.jpeg', 0, 0, 1000, 600);
-let tela_2 = new Telas('Imagens/fundo-cabana.jpeg', 0, 0, 1000, 600);
-
 let bancada_cozinha2 = new Objetos('Imagens/bancada-cozinha2.png', 900, 120, 100, 160);
-let fogao = new Objetos('Imagens/fogao.png', 802, 120, 100, 110)
+let fogao = new Objetos('Imagens/fogao.png', 802, 120, 100, 110);
 let bancada_cozinha1 = new Objetos('Imagens/bancada-cozinha1.png', 553, 120, 250, 160);
-let geladeira = new Objetos('Imagens/geladeira.png', 458, 80, 100, 150)
-let cama = new Objetos('Imagens/cama.png', 40, 90, 170, 228)
-let tapete = new Objetos('Imagens/tapete.png', 670, 250, 148, 99)
-let planta = new Objetos('Imagens/planta.png', 925, 230, 78, 113)
-let florzinha = new Objetos('Imagens/florzinha.png', 500, 300, 43, 74)
+let geladeira = new Objetos('Imagens/geladeira.png', 458, 80, 100, 150);
+let cama = new Objetos('Imagens/cama.png', 40, 90, 170, 228);
+let tapete = new Objetos('Imagens/tapete.png', 670, 250, 148, 99);
+let planta = new Objetos('Imagens/planta.png', 925, 230, 78, 113);
+let florzinha = new Objetos('Imagens/florzinha.png', 500, 300, 43, 74);
+let cadeira_jantar_frente1 = new Objetos('Imagens/cadeira_jantar_frente.png', 650, 335, 70, 130);
+let cadeira_jantar_frente2 = new Objetos('Imagens/cadeira_jantar_frente.png', 760, 335, 70, 130);
 
+let tela_1 = {
+    tela: new Telas('Imagens/praia.jpeg', 0, 0, 1000, 600),
+}
+let tela_2 = {
+    tela: new Telas('Imagens/fundo-cabana.jpeg', 0, 0, 1000, 600),
+    cenario: [bancada_cozinha2, bancada_cozinha1, fogao, geladeira, tapete, planta, cadeira_jantar_frente1, cadeira_jantar_frente2],
+    desenhe: function() {
+        this.tela.desenhe();
+        this.cenario.forEach(obj => obj.desenhe());
+    } 
+}
 
 function animation(){
     ctx.clearRect(0,0,canva1.width, canva1.height);
     tela_2.desenhe();
-    bancada_cozinha2.desenhe();
-    bancada_cozinha1.desenhe();
-    fogao.desenhe();
-    geladeira.desenhe();
-    tapete.desenhe();
-    planta.desenhe();
-
     florzinha.desenhe();
 
     requestAnimationFrame(animation)
